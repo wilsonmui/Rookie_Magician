e7a6cab955451cf882cef06042891d737f34088c
poopoomarvin <poopoomarvin@github.com>
2019-12-07 19:31:49 -0800

    word_matching

--- word_matching.py
+++ word_matching.py
@@ -1,7 +1,19 @@
 def word_matching(keywords, text):
     keywords = convert_keywords(keywords)
     text = convert_text(text)
-
+    matching_count = 0
+    for i in range(len(keywords)):
+        start_matching_index = 0
+        matching_keyword = keywords[i]
+        while(True):
+            start_matching_index = text.find(matching_keyword, start_matching_index)
+            if start_matching_index != -1: # find the keyword
+                start_matching_index += 1 # move on
+                matching_count += 1
+            else :
+                break
+                
+    return matching_count
 def convert_keywords(keywords):
     # add spaces to both front and end of each keywords
     for i in range(len(keywords)):
